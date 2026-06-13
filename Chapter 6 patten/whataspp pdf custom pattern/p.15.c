4f006f1c2960ba93ad862a33fcdc980dc889303c
VinitJaat304 <vinitkumae45@gmail.com>
2026-06-13 14:55:17 +0530

    pattern

---
+++ Chapter 6 patten/whataspp pdf custom pattern/p.15.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+main()
+{
+	int i;
+	
+	for(i=1; i<=7; i++)
+	{
+		if(i==1)
+			printf("* * * * *\n");
+		else if(i==2 ||i==3 || i==4 || i==5)
+			printf("    *\n");
+		else if(i==6)
+			printf("*   *\n");
+		else
+			printf(" * *\n");
+	}
+}
