b70f34238fc8a2421a2e70f8abd7d7d11e3bb1c9
VinitJaat304 <vinitkumae45@gmail.com>
2026-07-02 15:02:14 +0530

    GITHUB

---
+++ jay/p.1.c
@@ -0,0 +1,36 @@
+{
+    "workbench.iconTheme": "material-icon-theme",
+    "[html]": {
+        "editor.defaultFormatter": "vscode.html-language-features"
+    },
+    "[javascript]": {
+        "editor.defaultFormatter": "vscode.typescript-language-features"
+    },
+    "explorer.confirmDelete": false,
+    "editor.fontSize": 16,
+    "editor.mouseWheelZoom": true,
+    "code-runner.runInTerminal": true,
+    "workbench.editor.empty.hint": "hidden",
+    "terminal.integrated.defaultProfile.windows": "Command Prompt",
+    "files.autoSave": "afterDelay",
+    "http.systemCertificatesNode": true,
+    "geminicodeassist.project": "theta-amulet-psdh5",
+    "chat.viewSessions.orientation": "stacked",
+    "workbench.colorTheme": "Visual Studio 2017 Dark - C++",
+    "amazonQ.suppressPrompts": {
+        "amazonQChatPairProgramming": true,
+        "amazonQChatDisclaimer": true
+    },
+    "liveServer.settings.donotVerifyTags": true,
+    "editor.quickSuggestions": {
+    
+    
+        
+        "other": "offWhenInlineCompletions",
+        "comments": "off",
+        "strings": "off"
+    },
+    "diffEditor.codeLens": true,
+    "diffEditor.ignoreTrimWhitespace": false,
+    "editor.inlineSuggest.minShowDelay": 1000
+}
